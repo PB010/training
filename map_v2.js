f22a20689269ca2a19ee3d9e5a4cc158a6ec85e2
MFaurash <88086699+MFaurash@users.noreply.github.com>
2021-07-29 17:01:06 +0300

    Fixed function not being recognized

--- map_v2.js
+++ map_v2.js
@@ -1,5 +1,5 @@
 //
-Array.mymapv2 = function() {
+mymapv2 = function(callback) {
     const resultArray = [];
     for (let index = 0; index < sample.length; index++) {
         resultArray.push(callback(sample[index], index, sample));
@@ -9,7 +9,7 @@ Array.mymapv2 = function() {
 const sample = [1,2,3];
 var output = sample.mymapv2(function(val, index, array) {
     console.log('val :', val, 'index :', index, 'array ;', array);
-    return val*2
+    return (val * 2);
 })
 
 //
