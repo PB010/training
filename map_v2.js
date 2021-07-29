c8a9fee360bf31f78650fdbdb676800da48c3a17
MFaurash <88086699+MFaurash@users.noreply.github.com>
2021-07-29 21:42:37 +0300

    Add files via upload

--- map_v2.js
+++ map_v2.js
@@ -1,15 +1,15 @@
 //
-mymapv2 = function(callback) {
+Array.prototype.mymapv2 = function(callback) {
     const resultArray = [];
     for (let index = 0; index < sample.length; index++) {
         resultArray.push(callback(sample[index], index, sample));
     }
     return resultArray;
 }
-const sample = [1,2,3];
+const sample = [4,7,9];
 var output = sample.mymapv2(function(val, index, array) {
     console.log('val :', val, 'index :', index, 'array ;', array);
-    return (val * 2);
+    return val*2;
 })
-
+console.log(output);
 //
